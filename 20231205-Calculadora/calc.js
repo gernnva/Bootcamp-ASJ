e28afd1b7f2217e05025e8f8e420e306acb50442
German Nuñez <gnunez@petersen.corp>
2023-12-06 10:11:12 -0300

    solo dos numeros

--- 20231205-Calculadora/calc.js
+++ 20231205-Calculadora/calc.js
@@ -13,94 +13,101 @@ let num9 = document.getElementById("btn9");
 const borrar = document.getElementById("borrar");
 const sumar = document.getElementById("sumar")
 const igual = document.getElementById("igual")
-let valores = [];
+const restar = document.getElementById("restar");
+const multiplicar = document.getElementById("multiplicar");
+const dividir = document.getElementById("dividir");
+
+let valor1 = 0;
+let valor2 = 0;
 
 // FIN BOTONES
 // Variable que guarda el numero mostrado en pantalla
-let numEnPantalla = "";
+let numEnPantalla = 0;
 let sumaTotal = 0;
+let operador = "";
 
 //
-num0.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num0.textContent;
-    mostrarEnPantalla()
-})
 
-num1.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num1.textContent;
-    mostrarEnPantalla()
-})
+num0.addEventListener("click", () => agregarDigito(0));
+num1.addEventListener("click", () => agregarDigito(1));
+num2.addEventListener("click", () => agregarDigito(2));
+num3.addEventListener("click", () => agregarDigito(3));
+num4.addEventListener("click", () => agregarDigito(4));
+num5.addEventListener("click", () => agregarDigito(5));
+num6.addEventListener("click", () => agregarDigito(6));
+num7.addEventListener("click", () => agregarDigito(7));
+num8.addEventListener("click", () => agregarDigito(8));
+num9.addEventListener("click", () => agregarDigito(9));
 
-num2.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num2.textContent;
-    mostrarEnPantalla()
-})
-
-num3.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num3.textContent;
-    mostrarEnPantalla()
-})
-
-num4.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num4.textContent;
-    mostrarEnPantalla()
-})
-
-num5.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num5.textContent;
-    mostrarEnPantalla()
-})
-
-num6.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num6.textContent;
-    mostrarEnPantalla()
-})
-
-num7.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num7.textContent;
-    mostrarEnPantalla()
-})
-
-num8.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num8.textContent;
-    mostrarEnPantalla()
-})
-
-num9.addEventListener("click", () => {
-    numEnPantalla = numEnPantalla + num9.textContent;
-    mostrarEnPantalla()
-})
-
-borrar.addEventListener("click", () => borrarPantalla())
-
-sumar.addEventListener("click", () => {
-    //let valor1 = numEnPantalla
-    valores.push(parseFloat(numEnPantalla));
-    borrarPantalla()
-})
-
-igual.addEventListener("click", () => {
-    valores.forEach(element => { 
-        sumaTotal = sumaTotal + element;
-        
-    });
-    sumaTotal += parseFloat(numEnPantalla);
-    console.log(sumaTotal)
-    console.log(valores)
-    valores.splice(0, valores.length);
-    screen.textContent = sumaTotal;
-    console.log(valores)
-})
-
+function agregarDigito(digito) {
+    numEnPantalla = numEnPantalla + digito;
+    mostrarEnPantalla();
+}
 
 function mostrarEnPantalla () {
     screen.textContent = numEnPantalla;
 }
+
+borrar.addEventListener("click", () => borrarPantalla())
+
+sumar.addEventListener("click", () => {
+    valor1 = parseFloat(numEnPantalla);
+    operador = "sumar"
+    borrarPantalla();
+
+});
+
+restar.addEventListener("click", () => {
+    valor1 = parseFloat(numEnPantalla);
+    operador = "restar"
+    borrarPantalla();
+
+});
+
+multiplicar.addEventListener("click", () => {
+    valor1 = parseFloat(numEnPantalla);
+    operador = "multiplicar"
+    borrarPantalla();
+
+});
+
+dividir.addEventListener("click", () => {
+    valor1 = parseFloat(numEnPantalla);
+    operador = "dividir"
+    borrarPantalla();
+});
+
+igual.addEventListener("click", () => {
+    
+    let resultado = 0;
+
+    switch (operador) {
+        case "sumar":
+            resultado = valor1 + parseFloat(numEnPantalla);
+            break;
+        case "restar":
+            resultado = valor1 - parseFloat(numEnPantalla);
+            break;
+        case "multiplicar":
+            resultado = valor1 * parseFloat(numEnPantalla);
+            break;
+        case "dividir":
+            resultado = valor1 / parseFloat(numEnPantalla);
+            break;
+    }
+
+    screen.textContent = resultado;
+    valor1 = 0;
+    numEnPantalla = "";
+    operador = "";  
+})
 
 function borrarPantalla () {
     numEnPantalla = "";
     mostrarEnPantalla();
 }
 
-
+function vaciarArray () {
+    valores = [];
+}
 

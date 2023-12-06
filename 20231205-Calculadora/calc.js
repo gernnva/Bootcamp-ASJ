0a5c10d1ffae88bf4810e1280f6dac26cae8fcf1
German Nuñez <gnunez@petersen.corp>
2023-12-05 23:23:20 -0300

    botones y suma

--- 20231205-Calculadora/calc.js
+++ 20231205-Calculadora/calc.js
@@ -1,27 +1,106 @@
 let screen = document.getElementById("screen");
+    //BOTONES DE LA CALCULADORA
+let num0 = document.getElementById("btn0");
+let num1 = document.getElementById("btn1");
+let num2 = document.getElementById("btn2");
+let num3 = document.getElementById("btn3");
+let num4 = document.getElementById("btn4");
+let num5 = document.getElementById("btn5");
+let num6 = document.getElementById("btn6");
+let num7 = document.getElementById("btn7");
+let num8 = document.getElementById("btn8");
+let num9 = document.getElementById("btn9");
+const borrar = document.getElementById("borrar");
+const sumar = document.getElementById("sumar")
+const igual = document.getElementById("igual")
+let valores = [];
 
-let num0 = document.getElementById("btn0").value;
-let num1 = document.getElementById("btn1").value;
-let num2 = document.getElementById("btn2").value;
-let num3 = document.getElementById("btn3").value;
-let num4 = document.getElementById("btn4").value;
-let num5 = document.getElementById("btn5").value;
-let num6 = document.getElementById("btn6").value;
-let num7 = document.getElementById("btn7").value;
-let num8 = document.getElementById("btn8").value;
-let num9 = document.getElementById("btn9").value;
-
+// FIN BOTONES
+// Variable que guarda el numero mostrado en pantalla
 let numEnPantalla = "";
+let sumaTotal = 0;
 
+//
+num0.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num0.textContent;
+    mostrarEnPantalla()
+})
 
-function agregarNumero(num){
-    numEnPantalla = numEnPantalla + ;
-    console.log(numEnPantalla);
-    mostrarEnPantalla();
-}
+num1.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num1.textContent;
+    mostrarEnPantalla()
+})
+
+num2.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num2.textContent;
+    mostrarEnPantalla()
+})
+
+num3.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num3.textContent;
+    mostrarEnPantalla()
+})
+
+num4.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num4.textContent;
+    mostrarEnPantalla()
+})
+
+num5.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num5.textContent;
+    mostrarEnPantalla()
+})
+
+num6.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num6.textContent;
+    mostrarEnPantalla()
+})
+
+num7.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num7.textContent;
+    mostrarEnPantalla()
+})
+
+num8.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num8.textContent;
+    mostrarEnPantalla()
+})
+
+num9.addEventListener("click", () => {
+    numEnPantalla = numEnPantalla + num9.textContent;
+    mostrarEnPantalla()
+})
+
+borrar.addEventListener("click", () => borrarPantalla())
+
+sumar.addEventListener("click", () => {
+    //let valor1 = numEnPantalla
+    valores.push(parseFloat(numEnPantalla));
+    borrarPantalla()
+})
+
+igual.addEventListener("click", () => {
+    valores.forEach(element => { 
+        sumaTotal = sumaTotal + element;
+        
+    });
+    sumaTotal += parseFloat(numEnPantalla);
+    console.log(sumaTotal)
+    console.log(valores)
+    valores.splice(0, valores.length);
+    screen.textContent = sumaTotal;
+    console.log(valores)
+})
+
 
 function mostrarEnPantalla () {
     screen.textContent = numEnPantalla;
 }
+
+function borrarPantalla () {
+    numEnPantalla = "";
+    mostrarEnPantalla();
+}
+
 
 
